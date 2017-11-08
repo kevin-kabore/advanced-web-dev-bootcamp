8c9f15ccc56ac17570500ac3d0ce7174af3c6f98
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-11-08 11:08:14 -0800

    adds shorhand method examples

--- section-8-ajax-pt2-jquery-axios/examples/index.js
+++ section-8-ajax-pt2-jquery-axios/examples/index.js
@@ -13,3 +13,35 @@ $("#btn").click(function(){
 function addP(data) {
   $("#p").text(data[0])
 }
+
+
+$("#getBtn").click(function(){
+  $.get('https://api.github.com/users/colt')
+  .done(function(data){
+    console.log(data);
+  })
+  .fail(function(){
+    console.log("ERROR!");
+  })
+});
+
+$("#postBtn").click(function(){
+ var data = {name: "Charlie", city: "Florence"};
+ $.post("www.catsarecoolandsoaredogs.com", data)
+  .done(function(data){
+   console.log("HI!");
+ })
+  .fail(function(){
+   console.log("ERROR!");
+ })
+});
+
+$("#getJSONBtn").click(function(){
+  $.getJSON("https://api.github.com/users/colt")
+  .done(function(data){
+    console.log(data);
+  })
+  .fail(function(){
+    console.log("PROBLEM!");
+  })
+});
