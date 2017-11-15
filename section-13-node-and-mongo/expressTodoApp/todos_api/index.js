4631ad6d5457ad7ce89c5d82eb1c375bd28ddd22
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-11-14 16:28:08 -0800

    Creates post route

--- section-13-node-and-mongo/expressTodoApp/todos_api/index.js
+++ section-13-node-and-mongo/expressTodoApp/todos_api/index.js
@@ -1,6 +1,10 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var todoRoutes = require('./routes/todos');
 
--- section-13-node-and-mongo/expressTodoApp/todos_api/routes/todos.js
+++ section-13-node-and-mongo/expressTodoApp/todos_api/routes/todos.js
@@ -3,7 +3,25 @@ var router = express.Router();
 var db = require('../models');
 
 router.get('/', function(req, res) {
-  res.send('Inside Todo routes');
+  db.Todo
+    .find()
+    .then(function(todos) {
+      res.json(todos);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+});
+
+router.post('/', function(req, res) {
+  db.Todo
+    .create(req.body)
+    .then(function(newTodo) {
+      res.status(201).json(newTodo);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
 });
 
 module.exports = router;
