cc4f9bc424344d537670ea06dadf361f4510f2d0
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-11-14 21:18:53 -0800

    adds frontend post request

--- section-13-node-and-mongo/expressTodoApp/todos_api/public/app.js
+++ section-13-node-and-mongo/expressTodoApp/todos_api/public/app.js
@@ -1,13 +1,34 @@
 $(document).ready(function() {
   $.getJSON('/api/todos').then(addTodos);
+  $('#todoInput').keypress(function(event) {
+    if (event.which == 13) {
+      createTodo();
+    }
+  });
 });
 
 function addTodos(todos) {
   todos.forEach(function(todo) {
-    var newTodo = $('<li class="task">' + todo.name + todo.completed + '</li>');
-    if (todo.completed) {
-      newTodo.addClass('done');
-    }
-    $('.list').append(newTodo);
+    addTodo(todo);
   });
+}
+
+function addTodo(todo) {
+  var newTodo = $('<li class="task">' + todo.name + '</li>');
+  if (todo.completed) {
+    newTodo.addClass('done');
+  }
+  $('.list').append(newTodo);
+}
+
+function createTodo() {
+  var userInput = $('#todoInput').val();
+  $.post('/api/todos', { name: userInput })
+    .then(function(newTodo) {
+      $('#todoInput').val('');
+      addTodo(newTodo);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
