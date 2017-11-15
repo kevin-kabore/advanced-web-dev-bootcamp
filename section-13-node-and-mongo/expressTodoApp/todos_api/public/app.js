d0bb851cc4e3ca9d4ccc02e50fa515f27ad2100c
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-11-14 21:36:05 -0800

    completes delete method on front end using jquery data attribute

--- section-13-node-and-mongo/expressTodoApp/todos_api/public/app.js
+++ section-13-node-and-mongo/expressTodoApp/todos_api/public/app.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   $.getJSON('/api/todos').then(addTodos);
+
   $('#todoInput').keypress(function(event) {
     if (event.which == 13) {
       createTodo();
     }
+  });
+
+  $('.list').on('click', 'span', function() {
+    removeTodo($(this).parent());
   });
 });
 
@@ -14,7 +19,8 @@ function addTodos(todos) {
 }
 
 function addTodo(todo) {
-  var newTodo = $('<li class="task">' + todo.name + '</li>');
+  var newTodo = $('<li class="task">' + todo.name + ' <span>X</span></li>');
+  newTodo.data('id', todo._id);
   if (todo.completed) {
     newTodo.addClass('done');
   }
@@ -27,6 +33,21 @@ function createTodo() {
     .then(function(newTodo) {
       $('#todoInput').val('');
       addTodo(newTodo);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+function removeTodo(todo) {
+  var clickedId = todo.data('id');
+  var deleteUrl = '/api/todos/' + clickedId;
+  $.ajax({
+    method: 'DELETE',
+    url: deleteUrl
+  })
+    .then(function(data) {
+      todo.remove();
     })
     .catch(function(err) {
       console.log(err);
