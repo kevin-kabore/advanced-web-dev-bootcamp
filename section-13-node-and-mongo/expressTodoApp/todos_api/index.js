4b0f45f9a009b6a4e851f73d817b34a6a184da9d
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-11-14 20:29:29 -0800

    links css and js

--- section-13-node-and-mongo/expressTodoApp/todos_api/index.js
+++ section-13-node-and-mongo/expressTodoApp/todos_api/index.js
@@ -3,13 +3,15 @@ var express = require('express'),
   port = process.env.PORT || 3000,
   bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 var todoRoutes = require('./routes/todos');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
 app.get('/', function(req, res) {
-  res.send('Hello');
+  res.sendFile('index.html');
 });
 
 app.use('/api/todos', todoRoutes);
---
+++ section-13-node-and-mongo/expressTodoApp/todos_api/public/app.js
@@ -0,0 +1,1 @@
+alert('Alert from app.js');
