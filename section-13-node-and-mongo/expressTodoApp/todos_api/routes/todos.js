2a7f2570bb444c04a384b7d3f55db18332b29cbf
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-11-14 18:16:49 -0800

    adds show route

--- section-13-node-and-mongo/expressTodoApp/todos_api/routes/todos.js
+++ section-13-node-and-mongo/expressTodoApp/todos_api/routes/todos.js
@@ -24,4 +24,15 @@ router.post('/', function(req, res) {
     });
 });
 
+router.get('/:todoId', function(req, res) {
+  db.Todo
+    .findById(req.params.todoId)
+    .then(function(todo) {
+      res.json(todo);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+});
+
 module.exports = router;
