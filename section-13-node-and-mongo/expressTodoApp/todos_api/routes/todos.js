18b93a6145a0ea2b38d5894eb1919fff359146d3
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-11-14 18:27:21 -0800

    completes delete route

--- section-13-node-and-mongo/expressTodoApp/todos_api/routes/todos.js
+++ section-13-node-and-mongo/expressTodoApp/todos_api/routes/todos.js
@@ -46,4 +46,15 @@ router.put('/:todoId', function(req, res) {
     });
 });
 
+router.delete('/:todoId', function(req, res) {
+  db.Todo
+    .remove({ _id: req.params.todoId })
+    .then(function() {
+      res.json({ message: 'Todo was deleted' });
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+});
+
 module.exports = router;
