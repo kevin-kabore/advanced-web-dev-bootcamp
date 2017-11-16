42cedb92682a53c404e976edac9fd7e03edb2f98
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-11-16 11:39:13 -0800

    completes put method on frontend

--- section-13-node-and-mongo/expressTodoApp/todos_api/public/app.js
+++ section-13-node-and-mongo/expressTodoApp/todos_api/public/app.js
@@ -7,7 +7,12 @@ $(document).ready(function() {
     }
   });
 
-  $('.list').on('click', 'span', function() {
+  $('.list').on('click', 'li', function() {
+    updateTodo($(this));
+  });
+
+  $('.list').on('click', 'span', function(e) {
+    e.stopPropagation();
     removeTodo($(this).parent());
   });
 });
@@ -21,6 +26,7 @@ function addTodos(todos) {
 function addTodo(todo) {
   var newTodo = $('<li class="task">' + todo.name + ' <span>X</span></li>');
   newTodo.data('id', todo._id);
+  newTodo.data('completed', todo.completed);
   if (todo.completed) {
     newTodo.addClass('done');
   }
@@ -48,6 +54,25 @@ function removeTodo(todo) {
   })
     .then(function(data) {
       todo.remove();
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+function updateTodo(todo) {
+  // console.log(todo.data('completed'));
+  var updateUrl = '/api/todos/' + todo.data('id');
+  var isDone = !todo.data('completed');
+  var updateData = { completed: isDone };
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+    .then(function(updatedTodo) {
+      todo.toggleClass('done');
+      todo.data('completed', isDone);
     })
     .catch(function(err) {
       console.log(err);
