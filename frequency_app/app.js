d7599cb047bf63c7769b2f82e90a051c20248d3c
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-12-03 15:28:25 -0800

    adds text on top of rect

--- frequency_app/app.js
+++ frequency_app/app.js
@@ -1,11 +1,13 @@
 var phrase = d3.select('#phrase')
 var count = d3.select('#count')
 var input = d3.select('input')
+var width = 800
 var height = 400
-var width = 400
-var barWidth = 30
+var barWidth;
 var barPadding = 25
-
+var svg = d3.select('svg')
+                .attr('width', width)
+                .attr('height', height)
 
 d3.select('#reset').on('click', function(){
   d3.selectAll('.letter').remove()
@@ -20,6 +22,7 @@ d3.select('form').on('submit', function() {
   var text = input.property('value')
 
   var data = getFrequencies(text); // gets frequencies and returns sorted arr of obj
+  barWidth = width / data.length - barPadding
 
   setPhrase(text) // sets header phrase
   addLetters(data)
@@ -45,32 +48,48 @@ function setPhrase(text){
 
 
 function addLetters(data) {
-  var letters = d3.select('#letters') //update selection
-                  .attr('width', width)
-                  .attr('height', height)
-                    .selectAll('rect')
-                      .data(data, d => d.character) // key function to join by character
+  var letters = svg //update selection
+                  .selectAll('.letter')
+                  .data(data, d => d.character) // key function to join by character
   letters // exit selection - remove class of new
         .classed('new', false)
         .exit()
         .remove()
 
-  letters // enter selection - add class of new
+  var letterEnter = letters // enter selection - add class of new
         .enter()
-        .append('rect')
-        .classed('letter new', true)
-        .attr('width', barWidth)
-        .attr('height', function(d, i){
-          return d.count * 10
-        })
-        .attr('y', function(d, i){
-          return height - (d.count * 10)
-        })
+        .append('g')
+          .classed('letter new', true)
+
+  letterEnter.append('rect')
+  letterEnter.append('text')
+
+  letterEnter.merge(letters)
+    .select('rect')
+      .attr('width', barWidth)
+      .attr('height', function(d, i){
+        return d.count * 20
+      })
+      .attr('y', function(d, i){
+        return height - (d.count * 20)
+      })
+      .attr('x', function(d, i){
+        return (barWidth + barPadding) * i
+      })
+      .attr('fill', 'green')
+
+  letterEnter.merge(letters)
+      .select('text')
         .attr('x', function(d, i){
-          return (barWidth + barPadding) * i
+           return (barWidth + barPadding) * i + barWidth / 2;
         })
-        .attr('fill', 'green')
-        .merge(letters)
+        .attr('text-anchor', 'middle')
+        .attr('y', function(d){
+          return height - d.count * 20 - 10;
+        })
+        .text(function(d){
+          return d.character;
+        })
           // .style('width', '20px')
           // .style('line-height', '20px')
           // .style('margin-right', '5px')
