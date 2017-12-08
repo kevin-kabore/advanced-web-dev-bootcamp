5c8bf90653a19583bbec2f2609fcd83516bb7b64
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-12-08 14:05:51 -0800

    adds range input functionality

--- pie_chart_exercise/app.js
+++ pie_chart_exercise/app.js
@@ -1,7 +1,7 @@
 var width = 600;
 var height = 600;
 var minYear = d3.min(birthData, d => d.year);
-var yearData = birthData.filter(d => d.year === minYear);
+var maxYear = d3.max(birthData, d => d.year);
 
 var months = []
 
@@ -25,20 +25,49 @@ svg
     .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
     .classed('chart', true)
 
-var arcs = d3.pie()
-             .value(d => d.births)
-             (yearData)
+d3.select('input')
+    .property('min', minYear)
+    .property('max', maxYear)
+    .property('value', minYear)
+    .on('input', function(){
+      makeMonthGraph(+d3.event.target.value)
+    })
 
-var path = d3.arc()
-             .outerRadius(width / 2 - 10)
-             .innerRadius(width / 4);
 
-d3.select('.chart')
-  .selectAll('.arc')
-  .data(arcs)
-  .enter()
-  .append('path')
-    .classed('arc', true)
-    .attr('fill', d => colorScale(d.data.month))
-    .attr('stroke', 'black')
-    .attr('d', path);
+makeMonthGraph(minYear)
+
+function makeMonthGraph(year) {
+  var yearData = birthData.filter(d => d.year === year);
+
+  var arcs = d3.pie()
+               .value(d => d.births)
+               .sort(function(a, b){
+                 if (a.month < b.month) return -1
+                 else if (a.month > b.month) return 1
+               })
+               (yearData)
+
+  var path = d3.arc()
+               .outerRadius(width / 2 - 10)
+               .innerRadius(width / 4);
+
+  // select update selection and store
+  var update = d3.select('.chart')
+                 .selectAll('.arc')
+                 .data(arcs)
+
+  // exit selection and remove
+  update
+    .exit()
+    .remove()
+
+  // enter selection
+  update
+    .enter()
+    .append('path')
+      .classed('arc', true)
+    .merge(update)
+      .attr('fill', d => colorScale(d.data.month))
+      .attr('stroke', 'black')
+      .attr('d', path);
+}
