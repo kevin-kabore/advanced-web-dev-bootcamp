2a602e95ef0e84dbce382935b8ecab6fffdf095f
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-01-05 16:43:42 -0800

    completes frontend setup

--- section-31-authentication/warbler/warbler-server/helpers/messages.js
+++ section-31-authentication/warbler/warbler-server/helpers/messages.js
@@ -1,22 +1,36 @@
 var db = require('../models');
 
-exports.createMessage = function(req,res,next){
+exports.createMessage = function(req, res, next) {
+  console.log(req.body);
   const newMessage = {
     text: req.body.text,
     userId: req.params.id
   };
-  
-  db.Message.create(newMessage).then(function(message){
-    db.User.findById(req.params.id).then(function(user){
-      user.messages.push(message.id)
-      user.save().then(function(user) {
-        return db.Message.findById(message._id)
-          .populate("userId", {username: true, profileImageUrl: true})
-      }).then(function(m) {
-        return res.status(200).json(m);
-      }).catch(next);
-    }).catch(next);
-  }).catch(next);
+
+  db.Message
+    .create(newMessage)
+    .then(function(message) {
+      console.log('creating new message');
+      console.log(message);
+      db.User
+        .findById(req.params.id)
+        .then(function(user) {
+          user.messages.push(message.id);
+          user
+            .save()
+            .then(function(user) {
+              return db.Message
+                .findById(message._id)
+                .populate('userId', { username: true, profileImageUrl: true });
+            })
+            .then(function(m) {
+              return res.status(200).json(m);
+            })
+            .catch(next);
+        })
+        .catch(next);
+    })
+    .catch(next);
 };
 
 module.exports = exports;
